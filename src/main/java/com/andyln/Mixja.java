cd30a8352d36f70864834c6c329270ff9bcb249e
NguyenAndrew <anguyen29@wisc.edu>
2020-04-01 00:26:55 -0500

    Add Javadocs

--- src/main/java/com/andyln/Mixja.java
+++ src/main/java/com/andyln/Mixja.java
@@ -7,10 +7,24 @@ public class Mixja {
 
     private Mixja() {}
 
+    /**
+     * Creates an object that requires additional put/add/set/etc. Use a Java Lambda to setup and return this object.
+     *
+     * @param supplier Any Supplier
+     * @param <E> - Element returned by Supplier
+     * @return Return value of Supplier
+     */
     public static <E> E mix(Supplier<E> supplier) {
         return supplier.get();
     }
 
+    /**
+     * eix = checked (e)xception may be thrown m(ix). The performance of try-catch is why this method is standalone.
+     *
+     * @param callable Any Callable
+     * @param <E> - Element returned by Callable
+     * @return Return value of Callable
+     */
     public static <E> E eix(Callable<E> callable) {
         try {
             return callable.call();
